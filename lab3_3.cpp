838e4d1d490fb6e24dc1c5655181cc40be610a30
POOHPHE <37149762+POOHPHE@users.noreply.github.com>
2019-12-27 14:59:38 +0700

    Add files via upload

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -1,9 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-	int starto = 2;
+	int starto = 6;
 	double sum=0;
-	while(starto<70){
+	while(starto<19){
 		sum+=1.0/starto;
 		starto++;
 	}
